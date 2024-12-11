ac61fa0cad4f99a3ce6dc4e53814b79f93ccec0c
Henrik Jernbeck <henrik.jernbeck@gmail.com>
2024-12-11 11:41:30 +0100

    added some slop with pandas basic

--- Regression/project.py
+++ Regression/project.py
@@ -1,5 +1,10 @@
 import pandas as pd
 
-df = pd.DataFrame()
+
+df = pd.DataFrame(index=['A','B','C','D'], columns=['x','y','z'])
+df['x'] = [1,2,3,4] # column grab and assign
+df.loc['A'] = [1,2,3] # index grab and assign
+
+
 
 print(df)
